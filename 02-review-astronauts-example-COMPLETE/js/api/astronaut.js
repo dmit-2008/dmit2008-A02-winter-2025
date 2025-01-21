8761b2a7366e6c39d50d9c78e747c631f10128a6
Daniel Mouris <dgmouris@gmail.com>
2025-01-21 08:43:03 -0700

    day 3 progress

--- 02-review-astronauts-example-COMPLETE/js/api/astronaut.js
+++ 02-review-astronauts-example-COMPLETE/js/api/astronaut.js
@@ -1,3 +1,10 @@
 const BASE_URL = "https://lldev.thespacedevs.com/2.2.0"
 
 // api functions here.
+
+// change the BASE_URL to use version 2.3.0
+// create an async function
+// that's going to fetch all the data
+// return the data from the function
+// export that function and import in index.js
+// use that function print to the console.
