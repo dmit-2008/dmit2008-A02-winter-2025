1fdd4f1af735bb14485dbf472db13e8b5348fafe
Daniel Mouris <dgmouris@gmail.com>
2025-01-21 09:06:13 -0700

    day 3 progress

--- 02-review-astronauts-example-COMPLETE/js/api/astronaut.js
+++ 02-review-astronauts-example-COMPLETE/js/api/astronaut.js
@@ -1,10 +1,18 @@
-const BASE_URL = "https://lldev.thespacedevs.com/2.2.0"
+const BASE_URL = "https://lldev.thespacedevs.com/2.3.0"
 
 // api functions here.
 
 // change the BASE_URL to use version 2.3.0
 // create an async function
-// that's going to fetch all the data
 // return the data from the function
 // export that function and import in index.js
 // use that function print to the console.
+const getAstronautList = async () => {
+  const URL = `${BASE_URL}/astronauts/?mode=list`
+  // that's going to fetch all the data
+  const response = await fetch(URL, {
+    method: "GET" // this is the default for the options on the request
+    // normally here we add stuff like headers (authorization, content=type)
+    // as the body if it's a post or patch request.
+  })
+}
