1a159ca10bb72cb653d20ac142e27b8d57d3b225
Daniel Mouris <dgmouris@gmail.com>
2025-01-21 09:27:08 -0700

    day 3 progress

--- 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
+++ 02-review-astronauts-example-COMPLETE/js/dom/astronaut.js
@@ -15,10 +15,23 @@ replace the instances that have "THIS FORMAT HERE" with the astronautData.
 
 // create a function
 // that's going to two arguments
-// one is going to be the singleAstronautData
+// one is going to be the astronautData
 // second is going to be an element
-// I want you append the innerHTML
-// with the html above
+const renderAstronautListItem = (astronautData, element) => {
+  // I want you append the innerHTML
+  // with the html above
+  element.innerHTML += `<li href="#" class="list-group-item list-group-item-action">
+    <div class="d-flex w-100 justify-content-between">
+      <img src="PROFILE THUMBNAIL HERE" class="rounded float-start" alt=""">
+      <h5 class="mb-1">ASTRONAUT NAME (ASTRONAUT STATUS HERE)</h5>
+      <small class="float-end">born DATE OF BIRTH HERE</small>
+    </div>
+    <small>ASTRONAUT NATIONALITY HERE (ASTRONAUT AGENCY NAME)</small>
+    <p class="mb-1">ASTRONAUT BIO HERE </p>
+  </li>`
+}
+
 // export that function
+export { renderAstronautListItem }
 // import in the js
 // use it in the way that you think make sense.
--- 02-review-astronauts-example-COMPLETE/js/index.js
+++ 02-review-astronauts-example-COMPLETE/js/index.js
@@ -5,11 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 // we're going to import our api
 import { getAstronautList } from './api/astronaut'
+import { renderAstronautListItem } from './dom/astronaut'
 
-// when the document is oaded
+// when the document is loaded
 document.addEventListener("DOMContentLoaded", async () => {
   // get the data
   let data = await getAstronautList()
+  console.log(data)
   // get the name of the first astronaut in the results
   console.log(data.results[0].name)
+  // we're going to loop over the astronauts
+  let astronauts = data.results
+
 })
