3073fb560e45f8b0b1acc8edac8586cad95a4fe9
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 09:13:24 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-example/components/ConceptList.js
+++ 04-05-nextjs-components-intro/components-example/components/ConceptList.js
@@ -3,6 +3,13 @@
 // that wrap the children in an ol like in the index
 // export default
 // use this component in the index.js
+export default function ConceptList({children}) {
+  // we're just wrapping all of the children in an ordered list.
+  return <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+    {children}
+  </ol>
+}
+
 
 // Once done I want you to create a concept item component
 // is just a list item that takes a single called idea.
