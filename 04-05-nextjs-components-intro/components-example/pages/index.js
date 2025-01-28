6216f079f1289724a16354c5e4352a8e1debb8d1
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 08:54:00 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-example/pages/index.js
+++ 04-05-nextjs-components-intro/components-example/pages/index.js
@@ -1,4 +1,7 @@
 import Image from "next/image";
+// let's import the title
+import Title from "@/components/Title";
+
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -17,14 +20,8 @@ export default function Home() {
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
+        {/* Let's use our custom component here */}
+        <Title text={"Our first component"}/>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
