4cecf860f06f214c1915de25233c9ab763ee8e4d
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 09:07:59 -0700

    class progress pushed

---
+++ 04-05-nextjs-components-intro/components-example/components/ConceptList.js
@@ -0,0 +1,5 @@
+// I want you folks to create a components
+// called conceptlist that takes children as prop
+// that wrap the children in an ol like in the index
+// export default
+// use this component in the index.js
