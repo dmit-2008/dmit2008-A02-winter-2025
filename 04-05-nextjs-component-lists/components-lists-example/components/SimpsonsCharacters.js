bc66871c7eb82df7c93bdf65b90efa813469550d
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 09:40:34 -0700

    class progress pushed

--- 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
+++ 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
@@ -15,11 +15,20 @@ const SIMPSON_CHARACTERS = [
 // if you'd like try to use the style object to change
 // the color of the text.
 
-export default function SimpsonsCharacters() {
-  return <ul>
+export default function SimpsonsCharacters({color}) {
+  if (!color) {
+    color = "pink"
+  }
+
+  // I can pass in a color to change the color the text
+  // style is a prop that takes an object that essentially
+  // gives you the ability to change any css
+  return <ul style={{
+    color: color
+  }}>
     {SIMPSON_CHARACTERS.map((character, index)=> {
       // return a list of JSX items
-      return <li key={index}>
+      return <li key={index} className="mb-1">
         {character}
       </li>
     })}
