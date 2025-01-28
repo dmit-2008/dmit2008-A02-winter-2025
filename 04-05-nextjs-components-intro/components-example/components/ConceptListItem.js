723b62d68cc79666606206d479cc4c339ba081ef
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 09:17:42 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-example/components/ConceptListItem.js
+++ 04-05-nextjs-components-intro/components-example/components/ConceptListItem.js
@@ -1,5 +1,7 @@
 // Once done I want you to create a concept item component
 // is just a list item that takes a single called idea.
 export default function ConceptListItem({idea}) {
-
+  return <li>
+    {idea}
+  </li>
 }
