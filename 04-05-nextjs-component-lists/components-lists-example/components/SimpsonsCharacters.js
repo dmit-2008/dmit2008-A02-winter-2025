76f92aae86255558477efbd527aa3e9f39ae3d2e
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 09:30:15 -0700

    class progress pushed

---
+++ 04-05-nextjs-component-lists/components-lists-example/components/SimpsonsCharacters.js
@@ -0,0 +1,22 @@
+const SIMPSON_CHARACTERS = [
+	"Homer Simpson",
+	"Bart Simpson",
+	"Marge Simpson",
+	"Mr. Burns",
+	"Lisa Simpson",
+	"Apu Nahasapeemapetilon",
+	"Sideshow Bob",
+	"Milhouse Van Houten",
+	"Ned Flanders",
+]
+
+// I want you to use map to loop over the characters
+// define a key
+// if you'd like try to use the style object to change
+// the color of the text.
+
+export default function SimpsonsCharacters() {
+  return <ul>
+
+  </ul>
+}
