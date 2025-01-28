9f71befff9c90df89162d409f3adb4787fb08e00
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 08:46:39 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-example/components/TItle.js
+++ 04-05-nextjs-components-intro/components-example/components/TItle.js
@@ -2,5 +2,7 @@
 // called Title that takes a single prop
 // using obj destructuring called text
 // I want use default export and import in pages/index.js
-// and use it.
-// note this should wrap the text in an h1 in the jsx.
+export default function Title({text}) {
+  // note this should wrap the text in an h1 in the jsx.
+  return <h1>{text}</h1>
+}
