84fa424ee9aaa5d63778597c50e168bba601ef0d
Daniel Mouris <dgmouris@gmail.com>
2025-01-28 09:08:28 -0700

    class progress pushed

--- 04-05-nextjs-components-intro/components-example/components/ConceptList.js
+++ 04-05-nextjs-components-intro/components-example/components/ConceptList.js
@@ -3,3 +3,6 @@
 // that wrap the children in an ol like in the index
 // export default
 // use this component in the index.js
+
+// Once done I want you to create a concept item component
+// is just a list item that takes a single called idea.
