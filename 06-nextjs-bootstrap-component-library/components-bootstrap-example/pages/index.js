bf3cb5e2f18740582cd3a1a691987ef39655ea8d
Daniel Mouris <dgmouris@gmail.com>
2025-01-30 09:24:11 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
@@ -6,6 +6,8 @@
 // add two h2s in those columns
 
 // let's import conatiner docs https://react-bootstrap.netlify.app/docs/layout/grid/#container
+import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
@@ -25,11 +27,29 @@ export default function Home() {
           <Row>
             <Col>
               <h2>Available Libraries</h2>
+              {/* our custom components */}
               <UILibraryList />
             </Col>
             <Col>
               <h2>Other functionality</h2>
               {/* add a button and an accordion. */}
+              <Button variant="danger">Don't click me</Button>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>Idea #1</Accordion.Header>
+                  <Accordion.Body>
+                    That it's pretty easy to use our components and a
+                    component library side by side.
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Idea #2</Accordion.Header>
+                  <Accordion.Body>
+                    These component libraries have a lot built in that we can use
+                    in our own components so that we build some nest.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
             </Col>
           </Row>
         </Container>
