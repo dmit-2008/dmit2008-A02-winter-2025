0a2ebdb41056c0f0d1f6071bdfbf550d915b0421
Daniel Mouris <dgmouris@gmail.com>
2025-01-30 08:36:47 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/_app.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/_app.js
@@ -1,3 +1,12 @@
+/*
+This file is the entry point for all pages in our application
+
+We're going to import our react bootstrap into this file
+*/
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// we have a local styles file.
+// is we're going to change this file.
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
