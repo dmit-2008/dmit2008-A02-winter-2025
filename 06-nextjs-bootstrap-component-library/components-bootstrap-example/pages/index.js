0726b1bd146361d094ac1a8d52e20138250e5d9e
Daniel Mouris <dgmouris@gmail.com>
2025-01-30 08:47:58 -0700

    class progress pushed

--- 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
+++ 06-nextjs-bootstrap-component-library/components-bootstrap-example/pages/index.js
@@ -5,11 +5,27 @@
 // and two columns
 // add two h2s in those columns
 
+// let's import conatiner docs https://react-bootstrap.netlify.app/docs/layout/grid/#container
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+
 export default function Home() {
   return (
     <div>
       <main>
-
+        <Container>
+          <h1>My First React Boostrap Project</h1>
+          <Row>
+            <Col>
+              <h2>Available Libraries</h2>
+            </Col>
+            <Col>
+              <h2>Other functionality</h2>
+            </Col>
+          </Row>
+        </Container>
       </main>
     </div>
   );
