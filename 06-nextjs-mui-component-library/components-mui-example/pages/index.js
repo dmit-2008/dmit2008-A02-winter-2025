d720204dbc2c1a5b60be54ed2e75c1ebf06daf1c
Daniel Mouris <dgmouris@gmail.com>
2025-02-04 09:04:21 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/pages/index.js
+++ 06-nextjs-mui-component-library/components-mui-example/pages/index.js
@@ -1,5 +1,6 @@
-// import the container use the container component
+// import the container and use the container component
 // take a look at the "component" prop on the container component
+import Container from '@mui/material/Container';
 // use Typography component with a variant prop of h1
 // write the title "Using MUI"
 
