38a37760489600719306d796025572300b75a893
Daniel Mouris <dgmouris@gmail.com>
2025-02-04 09:46:28 -0700

    class progress pushed

--- 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
+++ 06-nextjs-mui-component-library/components-mui-example/components/SimpleForm.js
@@ -1,7 +1,14 @@
-// import and use a text field
-import { TextField } from "@mui/material"
-// import and use a checkbox with a label
 // import and use a button
+import Button from '@mui/material/Button';
+
+// import and use a checkbox with a label
+// refer to the docs here https://mui.com/material-ui/react-checkbox/
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+// import and use a text field
+import TextField from "@mui/material/TextField"
+
 // put this in a form in a SimpleForm
 // component no props.
 export default function SimpleForm() {
@@ -11,5 +18,15 @@ export default function SimpleForm() {
       label="Our demo text component"
       variant="outlined"
     />
+    {/* You can pass jsx as a prop */}
+    <FormGroup>
+      <FormControlLabel
+        control={<Checkbox defaultChecked />}
+        label="Check me"
+      />
+    </FormGroup>
+    <Button
+      variant="contained"
+    >Submit me</Button>
   </form>
 }
