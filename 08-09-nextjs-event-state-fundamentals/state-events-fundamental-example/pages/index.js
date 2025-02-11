b7ef65db67d90d86472889e700e56cb197c8e424
Daniel Mouris <dgmouris@gmail.com>
2025-02-11 09:35:42 -0700

    class progress pushed

--- 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
+++ 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
@@ -8,15 +8,21 @@
   second one will contain a button
   the grid will be instide a form elemnt
 */
+import { useState } from 'react'
+
+import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid2';
 import TextField from '@mui/material/TextField';
 
 
 export default function Home() {
+  // create a stateful value using use state
+  // called todoValue
+
+
   return (
-    <div
-    >
+    <div>
       <Container
         maxWidth="md"
         component="main"
@@ -27,18 +33,29 @@ export default function Home() {
           <Grid container spacing={2}>
             <Grid size={10}>
               {/* for our text field for the props you can always take a look
-              at the documentation for clarification. */}
+              at the documentation for clarification.
+
+              I want you to control the input
+              with this textfield.
+
+              */}
               <TextField
                 id="new-todo-input"
                 label="New Todo"
                 variant="outlined"
+                fullWidth
               />
             </Grid>
             <Grid size={2}>
               {/* for our button */}
+              <Button
+                variant="contained"
+                size="large"
+              >
+                Add Todo
+              </Button>
             </Grid>
           </Grid>
-
         </form>
       </Container>
     </div>
