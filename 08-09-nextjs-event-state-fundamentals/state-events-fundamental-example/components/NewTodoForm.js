97d321a76604f34fbcc47b4f36e963493f2ee8b4
Daniel Mouris <dgmouris@gmail.com>
2025-02-25 09:18:05 -0700

    class progress pushed

--- 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/components/NewTodoForm.js
+++ 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/components/NewTodoForm.js
@@ -1,3 +1,93 @@
 //  create the component
 // think of what needs to be a prop
 // what state can fully be in the form component.
+import {useState} from 'react'
+
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid2';
+import TextField from '@mui/material/TextField';
+
+export default function NewTodoForm({
+  todoList, setTodoList
+}) {
+  // create a stateful value using use state
+  // called todoValue
+  const [todoValue, setTodoValue] = useState("")
+
+  const updateTodoValue = (event) => {
+    // remember that the event.target will
+    // return the input in an onChange event
+    // and the .value is just what the user
+    // entered here.
+    setTodoValue(event.target.value)
+  }
+
+  // i want you to put an event listener
+  // that listens to the onSubmit event
+  // on the form and make a function that
+  // prints out the todoValue.
+  const handleForm = (event) => {
+    event.preventDefault()
+    // print the value of todo
+    console.log(`todoValue: ${todoValue}`)
+
+    // inside this form
+    // i'm going to create an copy of the array (think spread)
+    // with the todoValue as the first value
+    let tempList = [todoValue, ...todoList]
+    // includes all items of the todoList in the new array.
+    // we don't use push in this array because we need to make
+    // a copy because it's a const!
+    console.log(tempList)
+
+    // setTodoList with the new array
+    setTodoList(tempList)
+    // this will update the page!
+
+    // reset the value of the input
+    // we're going to setTodoValue to an empty string
+    setTodoValue("")
+  }
+
+  return <form
+    onSubmit={handleForm}
+  >
+    {/* we're going to use the grid */}
+    <Grid container spacing={2}>
+      <Grid size={10}>
+        {/* for our text field for the props you can always take a look
+          at the documentation for clarification.
+
+          I want you to control the input
+          with this textfield.
+            - put an onChange
+            - set the value to the todoValue
+          since this is synced with todoValue
+          it's called a "controlled input"
+        */}
+        <TextField
+          id="new-todo-input"
+          label="New Todo"
+          variant="outlined"
+          fullWidth
+          value={todoValue}
+          onChange={updateTodoValue}
+        />
+      </Grid>
+      <Grid size={2}>
+        {/* for our button */}
+        <Button
+          variant="contained"
+          size="large"
+          type="submit"
+        >
+          Add Todo
+        </Button>
+      </Grid>
+      {/* I'm going to make a grid size 12 */}
+      <Grid size={12}>
+
+      </Grid>
+    </Grid>
+  </form>
+}
--- 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
+++ 08-09-nextjs-event-state-fundamentals/state-events-fundamental-example/pages/index.js
@@ -10,12 +10,10 @@
 */
 import { useState } from 'react'
 
-import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid2';
-import TextField from '@mui/material/TextField';
 
 import ListOfTodos from '@/components/ListOfTodos';
+import NewTodoForm from '@/components/NewTodoForm';
 
 export default function Home() {
   const SAMPLE_TODOS = [
@@ -33,46 +31,6 @@ export default function Home() {
   // value is the SAMPLE_TODOS and you loop through
   // those todos in the JSX
   const [todoList, setTodoList] = useState(SAMPLE_TODOS)
-
-  // create a stateful value using use state
-  // called todoValue
-  const [todoValue, setTodoValue] = useState("fix wifi")
-
-  const updateTodoValue = (event) => {
-    // remember that the event.target will
-    // return the input in an onChange event
-    // and the .value is just what the user
-    // entered here.
-    setTodoValue(event.target.value)
-  }
-
-  // i want you to put an event listener
-  // that listens to the onSubmit event
-  // on the form and make a function that
-  // prints out the todoValue.
-
-  const handleForm = (event) => {
-    event.preventDefault()
-    // print the value of todo
-    console.log(`todoValue: ${todoValue}`)
-
-    // inside this form
-    // i'm going to create an copy of the array (think spread)
-    // with the todoValue as the first value
-    let tempList = [todoValue, ...todoList]
-    // includes all items of the todoList in the new array.
-    // we don't use push in this array because we need to make
-    // a copy because it's a const!
-    console.log(tempList)
-
-    // setTodoList with the new array
-    setTodoList(tempList)
-    // this will update the page!
-
-    // reset the value of the input
-    // we're going to setTodoValue to an empty string
-    setTodoValue("")
-  }
 
   // we're going to create a delete function
   // that's going to take an index as an argument
@@ -94,47 +52,14 @@ export default function Home() {
         component="main"
         sx={{mt: 8}}
       >
-        <form
-          onSubmit={handleForm}
-        >
-          {/* we're going to use the grid */}
-          <Grid container spacing={2}>
-            <Grid size={10}>
-              {/* for our text field for the props you can always take a look
-                at the documentation for clarification.
-
-                I want you to control the input
-                with this textfield.
-                  - put an onChange
-                  - set the value to the todoValue
-                since this is synced with todoValue
-                it's called a "controlled input"
-              */}
-              <TextField
-                id="new-todo-input"
-                label="New Todo"
-                variant="outlined"
-                fullWidth
-                value={todoValue}
-                onChange={updateTodoValue}
-              />
-            </Grid>
-            <Grid size={2}>
-              {/* for our button */}
-              <Button
-                variant="contained"
-                size="large"
-                type="submit"
-              >
-                Add Todo
-              </Button>
-            </Grid>
-            {/* I'm going to make a grid size 12 */}
-            <Grid size={12}>
-
-            </Grid>
-          </Grid>
-        </form>
+        {/* passing state down again
+          you can see the todolist is shared
+          state across both components below
+        */}
+        <NewTodoForm
+          todoList={todoList}
+          setTodoList={setTodoList}
+        />
         {/* pass down the stateful value of todoList
         pass down the function of deleteTodoItem
         so the component can use them. */}
