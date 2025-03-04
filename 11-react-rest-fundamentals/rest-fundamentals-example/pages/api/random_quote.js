fcd5ee38e5ac19245a32f1d5931b8efafbd2b0fd
Daniel Mouris <dgmouris@gmail.com>
2025-03-04 08:26:29 -0700

    class progress pushed

--- 11-react-rest-fundamentals/rest-fundamentals-example/pages/api/random_quote.js
+++ 11-react-rest-fundamentals/rest-fundamentals-example/pages/api/random_quote.js
@@ -2,5 +2,26 @@
 
 
 export default function handler(req, res) {
+  // a list of quotes
+  const QUOTE_LIST = [
+    {
+      "author": "Marcus Aurelius",
+      "quote": "Whatever happens to you has been waiting to happen since the beginning of time."
+    },
+    {
+      "author": "Seneca",
+      "quote": "A gift consists not in what is done or given, but in the intention of the giver or doer."
+    },
+    {
+      "quote": "I am not a demon. I am a lizard, a shark, a heat-seeking panther. I want to be Bob Denver on acid playing the accordion.",
+      "author": "The wise nicholas cage"
+    }
+  ]
+  // get a random quote from that list
+
+  // return that quote
+
+  // we're returning every request to /api/random_quote
+  // with states of 200 and the json of the argument
   res.status(200).json({ name: 'John Doe' })
 }
