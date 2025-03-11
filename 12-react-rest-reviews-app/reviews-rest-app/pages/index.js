5a1f1793b896442bfb3ca365c43c3d37366390c0
Daniel Mouris <dgmouris@gmail.com>
2025-03-11 09:41:37 -0600

    class progress pushed

--- 12-react-rest-reviews-app/reviews-rest-app/pages/index.js
+++ 12-react-rest-reviews-app/reviews-rest-app/pages/index.js
@@ -13,19 +13,8 @@ import CardContent from '@mui/material/CardContent';
 
 import Container from '@mui/material/Container';
 
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Grid from '@mui/material/Grid';
-
 import IconButton from '@mui/material/IconButton';
 
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-
-
-import TextField from '@mui/material/TextField';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -33,7 +22,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Navbar from '../components/Navbar';
 import NewReviewForm from '../components/NewReviewForm';
 
-import { getReviews, postReview, deleteReview } from '../utils/api/reviews';
+import { getReviews,  deleteReview } from '../utils/api/reviews';
 
 const MOCK_ADAPTATION_RATING = [{
   'title': 'Fight Club',
@@ -45,12 +34,6 @@ const BASE_URL = "http://localhost:5000"
 
 export default function Home() {
   // I want you to create the stateful values we discussed
-  const [title, setTitle] = useState("")
-  const [comments, setComments] = useState("")
-  // take a look the radio group we're going to
-  // initialize at "1" so it can be controlled
-  const [rating, setRating] = useState("1")
-
   // make the reviews stateful
   const [reviews, setReviews] = useState(MOCK_ADAPTATION_RATING)
 
@@ -75,58 +58,6 @@ export default function Home() {
       // display this error as a toast message.
       console.log(error)
     }
-  }
-
-  // handle the form submission
-  const handleForm = async (event) => {
-    event.preventDefault()
-    // validate so the input fields aren't empty.
-    if (title.trim() === "" || comments.trim() === "") {
-      // either one is empty we don't want to make the request
-      // so the function ends here.
-      return
-    }
-
-
-    // wrap this in a try catch.
-    try {
-
-      const newReview = await postReview({
-        title: title,
-        comments: comments,
-        rating: rating
-      })
-
-      console.log(newReview)
-
-      // after this we'll discuss state updates.
-      // Option 1: From our understanding of state
-      // updating the frontend stateful value
-      // setting reviews to a new array with the old values
-      // spread and the newReview in there.
-      // setReviews([...reviews, newReview])
-
-      // Option 2: cache clearing lite (calling loadReviews again)
-      // just call the function loadReviews, and this will make
-      // a separate api call to get the data from the endpoint that
-      // we've just changed (a lot of the time the post request above
-      // is considere a mutation).
-      await loadReviews() // we're not using any returned values.
-      // clear the inputs.
-      resetForm()
-    } catch (error) {
-      console.error(error)
-      // handle and display somethign to the user here.
-
-    }
-
-
-  }
-
-  const resetForm = () => {
-    setComments("")
-    setTitle("")
-    setRating("1")
   }
 
   // I want you to look up the card documentation
