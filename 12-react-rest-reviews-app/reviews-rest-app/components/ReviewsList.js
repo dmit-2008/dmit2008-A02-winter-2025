67d8242eaa9f87e205e50c3dcb52305c6dab40e0
Daniel Mouris <dgmouris@gmail.com>
2025-03-13 08:43:39 -0600

    class progress pushed

--- 12-react-rest-reviews-app/reviews-rest-app/components/ReviewsList.js
+++ 12-react-rest-reviews-app/reviews-rest-app/components/ReviewsList.js
@@ -1,7 +1,19 @@
-// I want you to create this.
+import ReviewCard from "./ReviewCard"
 
+
+// as an note we're passing down loadReviews and setReviews/reviews
+// to this component but we're not using it in this component.
+// this is call prop drilling and isn't the best.
+// we're goin to learn about the context api later in the course
+// and that's going to really help create state that is shared
+// across components.
 export default function ReviewsList({
-
+  reviews,
+  // if using option 2
+  loadReviews,
+  // if you're suing option 1
+  reviews,
+  setReviews
 }) {
 
   // Remember that for a component you can only return
