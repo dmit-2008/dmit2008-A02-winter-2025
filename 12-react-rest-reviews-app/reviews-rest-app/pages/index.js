985a20c121baa7d8657af9773e8c28de9f84323a
Daniel Mouris <dgmouris@gmail.com>
2025-03-13 08:32:28 -0600

    class progress pushed

--- 12-react-rest-reviews-app/reviews-rest-app/pages/index.js
+++ 12-react-rest-reviews-app/reviews-rest-app/pages/index.js
@@ -133,7 +133,12 @@ export default function Home() {
             </Button>
           </Box>
           {reviews.map((adaptation, index)=> {
+            // we need to add the key to the ReviewCard
+            // because we're looping it in JSX
+            // we need to uniquely identify it
+            // so we'll use the adaptation.id
             return <ReviewCard
+              key={adaptation.id}
               id={adaptation.id}
               title={adaptation.title}
               rating={adaptation.rating}
