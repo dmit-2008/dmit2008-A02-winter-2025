b4970eb8de4db1ca59e9a74f282f3bb3bb468cfb
Daniel Mouris <dgmouris@gmail.com>
2025-03-20 09:15:57 -0600

    class progress pushed

--- 16-lifecycle-hackernews/lifecycle-hackernews/components/TopArticlesList.js
+++ 16-lifecycle-hackernews/lifecycle-hackernews/components/TopArticlesList.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Button from "@mui/material/Button"
 
@@ -12,10 +12,29 @@ export default function TopArticlesList() {
     // slice (we're going to this bit more)
   const [slice, setSlice] = useState(1)
 
+  const loadTopStories = async () => {
+    // we're going to make the request to the backend
+    const TOP_STORIES_URL = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    const response = await fetch(TOP_STORIES_URL)
+    const data = await response.json()
+    setAllStoryIds(data)
+  }
 
   // an effect
     // load all of the stories
     // I want you to do this.
+  useEffect(()=> {
+    // remember that we're just invoking this function
+    // it's async but we're calling synchronously because of the
+    // restrictions of the useEffect not able to handle async functions.
+    loadTopStories()
+  }, [])
+
+  // let's take a look at allStoryIds
+  useEffect(()=> {
+    console.log(allStoryIds)
+  }, [allStoryIds])
+
 
   // jsx
     // a list of story components (we'll do that later)
