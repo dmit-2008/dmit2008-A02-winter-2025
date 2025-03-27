e4cf0da86d5d75585e91f1354a49e970dc8c16e7
Daniel Mouris <dgmouris@gmail.com>
2025-03-27 09:42:14 -0600

    class progress pushed

--- 19-nextjs-pages-and-space/space-pages/pages/agency/[agencyId].js
+++ 19-nextjs-pages-and-space/space-pages/pages/agency/[agencyId].js
@@ -8,10 +8,16 @@ import Container from '@mui/material/Container';
 import NavBar from '@components/NavBar';
 
 /*
-
+This file is going to be using the url path /agency/whateveridnumber
+beacuse our file path in pages is /agency/[agencyId].js
+What we'll do is we'll use the router to use the agencyId from
+router.query to display it.
 */
 export default function Agency() {
+  const router = useRouter()
 
+  // get the agencyId (again look at the file name)
+  const { agencyId } = router.query
 
   return (
     <div>
@@ -26,7 +32,8 @@ export default function Agency() {
         <Container sx={{paddingTop:2}} component="main" maxWidth="xs">
 
           <Typography variant="h3">
-            Agency
+            {/* Display the agency */}
+            Agency: { agencyId }
           </Typography>
           <Typography variant="p">
             This is a dynamic page.
