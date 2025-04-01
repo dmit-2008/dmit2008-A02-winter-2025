01fde14eb71f1aeb3ba0a0b7257451658005517e
Daniel Mouris <dgmouris@gmail.com>
2025-04-01 09:34:07 -0600

    class progress pushed

--- 19-nextjs-pages-and-space/space-pages/pages/spacecraft/[spacecraftId].js
+++ 19-nextjs-pages-and-space/space-pages/pages/spacecraft/[spacecraftId].js
@@ -1,4 +1,5 @@
-import Head from 'next/head'
+// same pattern as before
+import {useRouter} from 'next/router'
 
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -7,6 +8,13 @@ import Container from '@mui/material/Container';
 import NavBar from '@components/NavBar';
 
 export default function SpaceCraft() {
+  // declare the router
+  const router = useRouter()
+
+  // get the spacecraftId remember look at
+  // the filename with the name of the path
+  // parameter inside of the [ ].
+  const {spacecraftId} = router.query
 
   return (
     <div>
@@ -16,9 +24,10 @@ export default function SpaceCraft() {
         <Container sx={{paddingTop:2}} component="main" maxWidth="xs">
 
           <Typography variant="h3">
+            Spacecraft: {spacecraftId}
           </Typography>
           <Typography variant="p">
-
+            We'll finish this next class.
           </Typography>
         </Container>
 
