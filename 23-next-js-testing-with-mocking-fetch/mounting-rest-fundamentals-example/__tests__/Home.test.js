03d6e191a393f28a81de4edf6c63cd0bc1fa6f51
Daniel Mouris <dgmouris@gmail.com>
2025-04-15 09:42:31 -0600

    class progress pushed

--- 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/__tests__/Home.test.js
+++ 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/__tests__/Home.test.js
@@ -5,7 +5,7 @@ WE're mocking out the rest api.
   this package essentially bridge the gap
 - msw (docs https://mswjs.io/)
   - allows you to mock the backend of your application
-- jest-fixed-jsdom
+- jest-fixed-jsdom (https://github.com/mswjs/jest-fixed-jsdom)
   - next.js didn't take testing the api seriously
   - msw doesn't work perfect with next.js so we need to modify
   the jest.config.js to use this in their testEnvironment
@@ -20,3 +20,32 @@ import {render, act, screen} from '@testing-library/react'
 
 // the matchers
 import '@testing-library/jest-dom'
+
+// the component we're testing
+import Home from '../pages/index'
+
+// we're going to import the base url
+import { BASE_URL } from '../utils/api/base'
+
+// this is going to be used to create the mock endpoints
+// ref: https://mswjs.io/docs/getting-started#step-2-describe
+import { http, HttpResponse } from 'msw'
+
+// we're going to need to set up the server
+// ref: https://mswjs.io/docs/getting-started#step-3-integrate
+
+import { setupServer } from 'msw/node'
+
+
+// set up the mock endpoints
+
+
+
+// run the server before all of the tests
+
+// close the server after all of the tests
+
+
+// we're going to perform two tests
+// 1. check that quote loads on mount
+// 2. we're going to check a new quote is loaded when button is clicked.
