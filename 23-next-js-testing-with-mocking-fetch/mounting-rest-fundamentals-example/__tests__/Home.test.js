c69cfcafa8b7d6fe32fe93f8693477cf5a1082a4
Daniel Mouris <dgmouris@gmail.com>
2025-04-15 09:39:03 -0600

    class progress pushed

--- 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/__tests__/Home.test.js
+++ 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/__tests__/Home.test.js
@@ -1,4 +1,22 @@
 /*
-
+WE're mocking out the rest api.
+- isomorphic-fetch
+  - fetch on node and fetch on the browser are different
+  this package essentially bridge the gap
+- msw (docs https://mswjs.io/)
+  - allows you to mock the backend of your application
+- jest-fixed-jsdom
+  - next.js didn't take testing the api seriously
+  - msw doesn't work perfect with next.js so we need to modify
+  the jest.config.js to use this in their testEnvironment
 
 */
+import 'isomorphic-fetch'
+// make the node and browser fetch consistent
+
+// this used to render the component
+// complex state changes and select items with screen
+import {render, act, screen} from '@testing-library/react'
+
+// the matchers
+import '@testing-library/jest-dom'
--- 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/jest.config.js
+++ 23-next-js-testing-with-mocking-fetch/mounting-rest-fundamentals-example/jest.config.js
@@ -13,7 +13,9 @@ const customJestConfig = {
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
-  testEnvironment: 'jest-environment-jsdom',
+  // testEnvironment: 'jest-environment-jsdom',
+  // we're going to add the polyfill of the jest-fixed-dom
+  testEnvironment: 'jest-fixed-jsdom'
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
