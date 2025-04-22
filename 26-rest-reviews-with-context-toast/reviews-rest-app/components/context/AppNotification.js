1e02c8bb038964f6fe020eeb16b066907e99424e
Daniel Mouris <dgmouris@gmail.com>
2025-04-22 08:41:02 -0600

    class progress pushed

--- 26-rest-reviews-with-context-toast/reviews-rest-app/components/context/AppNotification.js
+++ 26-rest-reviews-with-context-toast/reviews-rest-app/components/context/AppNotification.js
@@ -3,10 +3,15 @@
 //    sometimes called "toast" message
 // 2. create the context in here and we're going to create
 //    a hook to simplify how we use this code.
-import {useState} from "react"
+import {useState, createContext} from "react"
+
 import Button from '@mui/material/Button';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
+
+// we're also going to export the context
+// this is going to "hold" the shared state.
+export const AppNotificationContext = createContext({})
 
 
 // we're going to wrap our app using this AppNotification
@@ -18,6 +23,15 @@ export default function AppNotification({children}) {
   // the message value.
   const [messageSeverity, setMessageSeverity] = useState("info")
   const [text, setText] = useState("")
+
+  // let's create a function that will change the state to open
+  // and set the text and severity thie will be with "message" and "type" respectively
+  const show = ({message, type}) => {
+    setMessageSeverity(type)
+    setText(text)
+
+    setOpen(true)
+  }
 
   // needed to handle our snackbar
   const handleClose = (event, reason) => {
@@ -33,19 +47,25 @@ export default function AppNotification({children}) {
   // so that we can change what kind of message
   // we display to the user.
   // docs here: https://mui.com/material-ui/react-snackbar/#use-with-alerts
-  return <>
+
+  // instead of a fragment we're going to add the provider
+  // what we're doing doing here is that we're passing the function show
+  // into an object so that it can imported into another component with
+  // useContext
+  return <AppNotificationContext.Provider value={{show}}>
     { // children is the entire app
     children
     }
+    {/* The snack is open only is "open" is true */}
     <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-        <Alert
-          onClose={handleClose}
-          severity={messageSeverity}
-          variant="filled"
-          sx={{ width: '100%' }}
-        >
-          {text}
-        </Alert>
-      </Snackbar>
-  </>
+      <Alert
+        onClose={handleClose}
+        severity={messageSeverity}
+        variant="filled"
+        sx={{ width: '100%' }}
+      >
+        {text}
+      </Alert>
+    </Snackbar>
+  </AppNotificationContext.Provider>
 }
