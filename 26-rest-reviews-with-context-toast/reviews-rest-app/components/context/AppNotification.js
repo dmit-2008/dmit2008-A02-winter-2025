15048fdec7a39f9feb466045dddea97ea18a7e5e
Daniel Mouris <dgmouris@gmail.com>
2025-04-22 08:24:53 -0600

    class progress pushed

---
+++ 26-rest-reviews-with-context-toast/reviews-rest-app/components/context/AppNotification.js
@@ -0,0 +1,10 @@
+// This is going to do two things
+// 1. it's going have the component that will raise the "snackbar"
+//    sometimes called "toast" message
+// 2. create the context in here and we're going to create
+//    a hook to simplify how we use this code.
+
+// we're going to wrap our app using this AppNotification
+export default function AppNotification({children}) {
+
+}
